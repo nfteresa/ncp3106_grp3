e95229597d9c3f1e4f978c3bcda636515da6e11c
nfteresa <141985953+nfteresa@users.noreply.github.com>
2023-11-30 08:13:41 +0800

    Update delete.php

--- ncp3106_grp3_php/Student_Registration/delete.php
+++ ncp3106_grp3_php/Student_Registration/delete.php
@@ -1,79 +1,118 @@
 <?php
-// Process delete operation after confirmation
-if (isset($_POST["id"]) && !empty($_POST["id"])) {
-    // Include config file
-    require_once "config.php";
-
-    // Prepare a delete statement
-    $sql = "DELETE FROM employees WHERE id = ?";
-
-    if ($stmt = $mysqli->prepare($sql)) {
-        // Bind variables to the prepared statement as parameters
-        $stmt->bind_param("i", $param_id);
-
-        // Set parameters
-        $param_id = trim($_POST["id"]);
-
-        // Attempt to execute the prepared statement
-        if ($stmt->execute()) {
-            // Records deleted successfully. Redirect to landing page
-            header("location: index.php");
-            exit();
-        } else {
-            echo "Oops! Something went wrong. Please try again later.";
+    if(isset($_POST['ids']) && !empty($_POST['ids'])){
+        //this contains all the values of the checkboxes that were ticked at form submission.
+        $ids = $_POST["ids"];
+        $url_ids = "";
+        //package array into string
+        while (count($ids)) {
+            $temp = array_pop($ids);
+            $url_ids = $url_ids . $temp . ",";
         }
+        // send user to delete_confirmation.php with the ids url-encoded.
+        header("location: delete_confirmation.php?ids=".urlencode($url_ids));
+    } else {
+        // error message here
+        echo "Something went wrong. Please try again later.";
     }
-
-    // Close statement
-    $stmt->close();
-
-    // Close connection
-    $mysqli->close();
-} else {
-    // Check existence of id parameter
-    if (empty(trim($_GET["id"]))) {
-        // URL doesn't contain id parameter. Redirect to error page
-        header("location: error.php");
-        exit();
-    }
-}
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
-    <title>Delete Record</title>
+    <title>Dashboard</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <style>
         .wrapper {
-            width: 600px;
+            width: 900px;
             margin: 0 auto;
         }
+
+        table tr td:last-child {
+            width: 120px;
+        }
     </style>
+    <script>
+        $(document).ready(function() {
+            $('[data-toggle="tooltip"]').tooltip();
+        });
+
+        function confirm() {
+            var array = [];
+            var checkboxes = document.querySelectorAll('input[type=checkbox]:checked');
+
+            for (var i = 0; i < checkboxes.length; i++) {
+                array.push(checkboxes[i].value);
+            }
+
+            return checkboxes;
+        }
+    </script>
+
 </head>
 
 <body>
     <div class="wrapper">
         <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-12">
-                    <h2 class="mt-5 mb-3">Delete Record</h2>
-                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-                        <div class="alert alert-danger">
-                            <input type="hidden" name="id" value="<?php echo trim($_GET["id"]); ?>" />
-                            <p>Are you sure you want to delete this employee record?</p>
-                            <p>
-                                <input type="submit" value="Yes" class="btn btn-danger">
-                                <a href="index.php" class="btn btn-secondary ml-2">No</a>
-                            </p>
-                        </div>
-                    </form>
-                </div>
-            </div>
+            <!-- the whole table as well as the button at the bottom is a form -->
+            <a href="index.php"><button class="btn btn-danger">Back</button></a>
+            <form method = "post">
+                <?php
+                require_once '../config.php';
+
+                $sql = "SELECT * FROM stud_info";
+                if ($result = $mysqli->query($sql)) {
+                    if($result->num_rows > 0) {
+                        echo '<table class="table table-bordered table-striped">';
+                        echo "<thead>";
+                        echo "<tr>";
+                        echo "<th>amongus</th>";
+                        echo "<th>#</th>";
+                        echo "<th>first_name</th>";
+                        echo "<th>last_name</th>";
+                        echo "<th>middle_initial</th>";
+                        echo "<th>student_number</th>";
+                        echo "<th>program</th>";
+                        echo "<th>current_year</th>";
+                        echo "<th>ue_email</th>";
+                        echo "<th>contact_number</th>";
+                        echo "</tr>";
+                        echo "</thead>";
+                        echo "<tbody>";
+                        while ($rows = $result->fetch_array()) {
+                            echo "<tr>";
+                            echo '<td><input type ="checkbox" name = "ids[]" id = "'. $rows["stud_id"].' "value = "'. $rows["stud_id"].'"></input></td>';
+                            echo "<td>" . $rows['stud_id'] . "</td>";
+                            echo "<td>" . $rows['first_name'] . "</td>";
+                            echo "<td>" . $rows['last_name'] . "</td>";
+                            echo "<td>" . $rows['middle_initial'] . "</td>";
+                            echo "<td>" . $rows['student_number'] . "</td>";
+                            echo "<td>" . $rows['program'] . "</td>";
+                            echo "<td>" . $rows['current_year'] . "</td>";
+                            echo "<td>" . $rows['ue_email'] . "</td>";
+                            echo "<td>" . $rows['contact_number'] . "</td>";
+                            echo "</tr>";
+                        }
+                        echo "</tbody>";
+                        echo "</table>";
+                        // Free result set
+                        $result->free();
+
+                    } else {
+
+                    }
+                } else {
+
+                }
+                ?>
+                <input type="submit" value="Delete"/>
+            </form>
         </div>
     </div>
 </body>
-
-</html>
