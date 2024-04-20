1108aa496a93f642238a84221c85de745cd2c356
ianueph <111487555+ianueph@users.noreply.github.com>
2024-04-20 20:53:18 +0800

    Update registration.php

--- ncp3106_grp3_php/Attendee_Registration/registration.php
+++ ncp3106_grp3_php/Attendee_Registration/registration.php
@@ -7,10 +7,9 @@
     $first_name = "";
     $last_name = "";
     $middle_initial = "";
-    $time_type = "";
+    
+
     $event_id = urldecode($_GET["event_id"]);
-    $id = "";
-    $time_out_err = "";
 
     $sql = "SELECT * FROM event_info WHERE event_id = ?";
     if ($stmt = $mysqli->prepare($sql)) {
@@ -28,101 +27,45 @@
     
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $input_time = trim($_POST["time_type"]);
         $input_student_number = trim($_POST["student_number"]);
-        $u = "SELECT student_number FROM attendees WHERE student_number ='$input_student_number' AND event_id='$event_id' AND time_in IS NOT NULL";
+        $u = "SELECT student_number FROM attendees WHERE student_number ='$input_student_number' AND event_id='$event_id'";
         $uu = $mysqli->query($u);
         $u = "SELECT student_number FROM student_info WHERE student_number ='$input_student_number'";
         $uu2 = $mysqli->query($u);
         if (empty($input_student_number)) {
-            $student_number_err = "WARNING: Invalid QR Pass!";
+            $student_number_err = "Please enter your student number.";
         } elseif (mysqli_num_rows($uu2) < 1 ) {
-            $student_number_err = "WARNING: QR Pass is not registered! <br> Please approach the event admin.";
-        } elseif ((mysqli_num_rows($uu) > 0) && ($input_time == "In")) {
-            $student_number_err = "WARNING: QR Pass already used. <br> Please approach the event admin.";
-        } elseif (True) { //nag True muna ako paki ayos pls.
+            $student_number_err = "Student number does not exist in database";
+        } elseif (mysqli_num_rows($uu) > 0 ) {
+            $student_number_err = "Student number already exists.";
+        } elseif (preg_match('/^[0-9]{11}+$/', $input_student_number)) {
             $student_number = $input_student_number;       
         }else{
             $student_number_err = "Please enter a valid student number.";
         }
 
-        if (($input_time == "Out") && empty($student_number_err)) {
-          $u = "SELECT id FROM attendees WHERE time_in IS NOT NULL AND time_out IS NULL AND student_number = '".$student_number."' AND event_id = '".$event_id."'";
-          $uu3 = $mysqli->query($u);
-          if ((mysqli_num_rows($uu3) > 0) && $input_time == "Out") {
-            $time_out_err = "";
-            $id = $uu3->fetch_array()["id"];
-          } elseif ((mysqli_num_rows($uu3) == 0) && $input_time == "Out"){
-            $time_out_err = "Please time in first.";
-          } else {
-            $time_out_err = "";
-          }
-        } else {
-          $time_out_err = "";
-        }
-        
-        if (empty($student_number_err) && ($input_time=="In") && empty($time_out_err)) { 
-            $dt = new DateTime("now", new DateTimeZone('Asia/Hong_Kong')); 
-            $sql = "INSERT INTO attendees (student_number, event_id, payment, time_in) VALUES (?, ?, ?, ?)";
+        if (empty($student_number_err)) {   
+            $sql = "INSERT INTO attendees (student_number, event_id, payment) VALUES (?, ?, ?)";
 
             if ($stmt = $mysqli->prepare($sql)) {
-                $stmt->bind_param("isis", $param_student_number, $param_event_id, $param_payment, $param_time_in);
+                $stmt->bind_param("isi", $param_student_number, $param_event_id, $param_payment);
 
                 $param_payment = $payment;
                 $param_event_id = $event_id;
                 $param_student_number = $student_number;
-                $param_time_in = $dt->format('H:i:s');
 
                 if ($stmt->execute()) {
-                    header("location: registration.php?event_id=".urlencode($event_id)."&payment=".urlencode($payment)."&sn=".urlencode($student_number))."&time_type=".urlencode($input_time);
-                    echo $input_time;
+                    header("location: registration.php?event_id=".urlencode($event_id)."&payment=".urlencode($payment)."&sn=".urlencode($student_number));
                 } else {
                     echo "something went wrong ):";
                 }
             }
             $stmt->close();
-        } elseif (empty($student_number_err) && ($input_time == "Out") && empty($time_out_err)) {
-            $dt = new DateTime("now", new DateTimeZone('Asia/Hong_Kong')); 
-            $time_in = "";
-            $sql = "SELECT time_in FROM attendees WHERE id = ? AND event_id = ?";
-            if ($stmt = $mysqli->prepare($sql)) {
-              $stmt->bind_param("ii", $param_id, $param_event_id);
-
-              $param_id = $id;
-              $param_event_id = $event_id;
-
-              if ($stmt->execute()) {
-                $time_in = new DateTime($stmt->get_result()->fetch_array()[0], new DateTimeZone('Asia/Hong_Kong'));
-              } else {
-                echo "couldn't get time_in from id";
-              }
-            }
-
-
-            $sql = "UPDATE attendees SET time_out = ?, time_attended = ? WHERE id = ? AND event_id = ?";
-
-            if ($stmt = $mysqli->prepare($sql)) {
-              $stmt->bind_param("ssii", $param_time_out, $param_time_attended, $param_id, $param_event_id);
-
-              $param_time_out = $dt->format('H:i:s');
-              $param_id = $id;
-              $param_event_id = $event_id;
-              $param_time_attended = ($dt->diff($time_in))->format("H:i:s");
-
-              if ($stmt->execute()) {
-                header("location: registration.php?event_id=".urlencode($event_id)."&payment=".urlencode($payment)."&sn=".urlencode($student_number)."&time_type=".urlencode($input_time));
-              } else {
-                echo "something went wrong ):";
-              }
-            }
-        } elseif (!empty($time_out_err) || !empty($student_number_err)) {  
-          // nothing happens :) because errors will be displayed
         } else {
-          echo "Something went wrong!"; 
         }
     } 
     $sql = "SELECT * FROM student_info WHERE student_number = ?";
-    if (!empty($_GET["sn"]) && empty($student_number_err) && empty($time_out_err)) {
+    if (!empty($_GET["sn"]) && empty($student_number_err)) {
       if ($stmt = $mysqli->prepare($sql)) {
         $stmt->bind_param('s',$param_student_number);
         $param_student_number = urldecode($_GET['sn']);
@@ -134,11 +77,6 @@
             $middle_initial = $result["middle_initial"];
         }
       } 
-    }
-    if (!empty($_GET["time_type"]))  {
-      $input_time = trim($_GET["time_type"]);
-    } else {
-      $input_time = "";
     }
     $mysqli->close();
 ?>
@@ -165,33 +103,30 @@
 <style>
   @font-face {
         font-family: myFirstFont;
-        src: url("../font/Poppins-Bold.ttf");
-  }
-  @font-face {
-        font-family: mySecondFont;
-        src: url("../font/Poppins-Black.ttf");
+        src: url("../font/Montserrat-VariableFont_wght.ttf");
   }
   body {
     font-family: myFirstFont;
-    background-image: url("./img/bg.png");
+    background-image: url("./img/bg4.png");
     background-size: cover;
     height: 100vh;
     overflow:hidden;
   }
   .container{
     display: flex;
-    margin: 0;
-    padding: 40px, 0px;
+    margin: auto;
+    padding: 40px 0;
   }
   .left-box{
     width: 100%;
     box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3); 
     border-radius: 10px 0px 0px 10px; 
-    height: 135%;
-    background-size:100% 100%;
-    background-repeat: no-repeat;
-    background-image:url('./img/background.png');
-    max-height: 70vh;
+    height: 100%;
+    background-size:cover ;
+    background-image:url('./img/left.png');
+    justify-content: center; 
+    align-items: center; 
+    max-height: 85vh;
     max-width: 85vw;
   }
   .left{
@@ -200,8 +135,7 @@
   }
   .left h1,p{
     font-family: myFirstFont;
-    color: #161F34;
-    font-weight:bold;
+    color: #013365;
   }
   .left-box a{
     position: absolute; 
@@ -210,19 +144,24 @@
     width:50px; 
     height: 50px;
   }
+  .left h1{
+    font-weight:bold;
+  }
   .right-box{
     width: 100%;
-    height: 125%;
+    height: 100%;
     box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3); 
     border-radius: 0px 10px 10px 0px; 
     background: white;
+    justify-content: center; 
+    align-items: center; 
     margin: 0;
-    max-height: 70vh;
+    max-height: 85vh;
     max-width: 85vw;
     overflow: scroll;
   }
   .right{
-    padding: 20px;
+    padding: 50px;
     line-height: 1.8;
     margin: 0 auto;
     text-align: center;
@@ -241,6 +180,7 @@
   .right form{
     color: black;
     border-radius: 3px;
+    margin-bottom: 15px;
     width: 100%;
     line-height: 1.8; 
   }
@@ -248,6 +188,7 @@
     background-color: #013365; 
     border: #013365 solid;
     font-family: myFirstFont;
+    font-weight: bold;
     outline: none !important;
   }
   .left img{
@@ -263,8 +204,12 @@
     <div class="container">
       <div class="col-md-6 px-0">
         <div class="left-box">
-        <a href="index.php"><img src="./img/back.png" style="opacity: 0.0;position: absolute; top: 8px; left: 16px; width:50px;height: 50px;"></a>
+          <a href="index.php"><img src="./img/back.png" style="position: absolute; top: 8px; left: 16px; width:50px;height: 50px;"></a>
           <div class="left">
+            <p style="font-weight:bold; font-size:30px"><?php echo $event_name ?></p>
+            <p><?php echo $event_description ?></p>
+            <p style="font-weight:bold; font-size:25px">Registration Fee</p>
+            <p><?php echo $payment;?></p>
           </div>
         </div>
       </div>
@@ -273,44 +218,22 @@
           <div class="right container-fluid">
             <form method="post">
               <div class="form-group">
-                <p class="fw-bold" style="font-weight:normal; font-size:45px; font-family:mySecondFont">Scan your QR Pass
+                <p style="font-weight:bold; font-size:25px">Scan for payment on <?php echo $event_name ?></p>
+                <img src="./img/qr.png"/>
               </div>
               <div class="form-group">
-                <select style="width:350px; margin: 0 auto; margin-left: auto; margin-right: auto; text-align:center; " name="time_type" class="form-control" value="<?php echo $_POST["time_type"]; ?>">
-                <?php
-                  $in = "<option value='In' style='font-weight: bold; color: #161F34;'>Time in</option>";
-                  $out = "<option value='Out' style='font-weight: bold; color: #161F34;'>Time out</option>";
-                  echo $_GET["time_type"];
-
-                  if ($_GET["time_type"] == "Out") {
-                    echo $out;
-                    echo $in;
-                  } else {
-                    echo $in;
-                    echo $out;
-                  }
-                ?>
-                </select>
-                  <br>
-                <input type="text" style="background-image: none; color: black; font-size:50pt; width:350px; margin: 0 auto; margin-left: auto; margin-right: auto; text-align:center;" name="student_number" class="form-control" value="<?php echo $student_number?>" autofocus/>
-                <p style="font-size:20px; color:red"> <?php echo $student_number_err; ?> </p>
-                <p style="font-size:20px; color:red"> <?php echo $time_out_err; ?> </p>
+                <h5 style="font-weight: bold; color: #013365;">Student Number</h5>
+                <input type="number" name="student_number" class="form-control <?php echo (!empty($student_number_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $student_number?>"/>
+                <span class="invalid-feedback"> <?php echo $student_number_err; ?> </span>
                 <span class=""> <?php 
-                  if (!empty($last_name) && !empty($first_name) && (empty($input_time))) {
-                    echo "<br><p style='font-size:20px;line-height: 0.7'>Welcome to <span style='color: red;'>UE</span> Tech Con 2024!,";
-                    echo "<p style='font-size:30px; line-height: 1;'>".$last_name.", ".$first_name." ".$middle_initial."."; 
-                  } elseif (!empty($last_name) && !empty($first_name) && (!empty($input_time))) {
-                    echo "<br><p style='font-size:20px;line-height: 0.7'>Thank you for coming to <span style='color: red;'>UE</span> Tech Con 2024!,";
-                    echo "<p style='font-size:30px; line-height: 1;'>".$last_name.", ".$first_name." ".$middle_initial."."; 
+                  if (!empty($last_name) && !empty($first_name)) {
+                    echo "".$last_name.", ".$first_name." ".$middle_initial.""; 
                   } else {
-
                   }
                   ?> </span>
-                <br>
-                <p style="font-size:15px; opacity: 0.0; line-height:02">brhrbr</p>
               </div>
-              <div style="">
-                <p style="bottom: 0; line-height: 1.3; font-size: 20px;">Happy Learning, future <br><span style="font-family:mySecondFont">Computer Engineers!</span></p>
+              <div class="form-group">
+                <button type="submit" class="btn btn-primary">Submit</button>
               </div>
               </form>
           </div>
@@ -320,4 +243,5 @@
   </div>
 </body>
 </html>
+
 
